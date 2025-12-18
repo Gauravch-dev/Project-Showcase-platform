c6f51550b74c86ccd27a0e2fc1205e85d10a0938
Gaurav-ch <gauravlan.ch@gmail.com>
2025-12-19 01:38:31 +0530

    style(layout): add header and footer components

---
+++ components/common/footer.tsx
@@ -0,0 +1,12 @@
+import { InstagramIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="border-t bg-muted/20 py-12">
+        <div className="wrapper">
+            Project Showcase Platform. All rights reserved
+        </div>
+    </footer>
+  )
+}
